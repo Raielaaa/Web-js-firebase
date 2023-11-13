52a810a699e52382cf89d0ceea4ba8a3c3143c39
Ralph Daniel Honra <rbhonra@ccc.edu.ph>
2023-11-13 17:41:40 +0800

    Fixed DB connection

--- index.js
+++ index.js
@@ -1,61 +1,59 @@
 // index.js
 
 // Import necessary Firebase modules
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getFirestore, collection, onSnapshot } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
 
 // Your Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCzZQNV6vZIG_h_p1-AU-vM1NWxZUNchmA",
-    authDomain: "qr-scanner-practice-3.firebaseapp.com",
-    databaseURL: "https://qr-scanner-practice-3-default-rtdb.firebaseio.com",
-    projectId: "qr-scanner-practice-3",
-    storageBucket: "qr-scanner-practice-3.appspot.com",
-    messagingSenderId: "818401297745",
-    appId: "1:818401297745:web:e7721cfd8052765df7ff70",
-    measurementId: "G-X1RFXKFD4B"
+    apiKey: "AIzaSyAiRIPC8QvOZQGeMEF2sP6uruXoTggFRsE",
+    authDomain: "ccc-library-system.firebaseapp.com",
+    projectId: "ccc-library-system",
+    storageBucket: "ccc-library-system.appspot.com",
+    messagingSenderId: "319826207112",
+    appId: "1:319826207112:web:05afc7f940382326541a3d",
+    measurementId: "G-VVLGDHLTTV"
 };
 
 // Initialize Firebase app
 const app = initializeApp(firebaseConfig);
 
-// Get a reference to the database
-const database = getDatabase(app);
+// Get a reference to the Firestore database
+const db = getFirestore(app);
 
-// Function to fetch and display data from Firebase
-function fetchData() {
-    // Reference to the 'Book-info' node in the database
-    const booksRef = ref(database, 'Book-info');
+function fetchBorrowData() {
+    // Reference to the 'ccc-library-borrow-data' collection
+    const borrowDataCollection = collection(db, 'ccc-library-app-borrow-data');
 
     // Attach an event listener to listen for changes in the data
-    onValue(booksRef, (snapshot) => {
+    onSnapshot(borrowDataCollection, (querySnapshot) => {
         // Clear existing content
         document.getElementById('txtPlaceholder').innerHTML = '';
 
-        // Iterate through the snapshot to get each book's data
-        snapshot.forEach((childSnapshot) => {
-            const bookData = childSnapshot.val();
+        // Iterate through the query snapshot to get each document's data
+        querySnapshot.forEach((doc) => {
+            const data = doc.data();
 
-            // Display the book data on the website
+            // Display the data on the website
             document.getElementById('txtPlaceholder').innerHTML += `
-                <p>Book Name: ${bookData.bookName}, Author: ${bookData.bookAuthor}</p>
+                <p>userID: ${data.userID} | Book Name: ${data.modelBookName} | Author: ${data.modelBookAuthor} | Borrower: ${data.modelBorrower} | Status: ${data.modelBorrowStatus} | Book code: ${data.modelBookCode} | Genre: ${data.modelBookGenre} | Borrow data: ${data.modelBorrowDate} | Borrow status: ${data.modelBorrowStatus} | Borrow deadline: ${data.modelBorrowDeadline} | Program: ${data.modelProgram} | Section: ${data.modelSection}, Year: ${data.modelYear}</p>
             `;
-            console.log("Book Name: ${bookData.bookName}, Author: ${bookData.bookAuthor}")
+            console.log(`Book Name: ${data.modelBookName}, Author: ${data.modelBookAuthor}`);
         });
     });
 }
 
 // Initial fetch when the page loads
-fetchData();
+fetchBorrowData();
 
 // Function to automatically update the content when the database changes
 function setupDatabaseListener() {
-    const booksRef = ref(database, 'Book-info');
+    const borrowDataCollection = collection(db, 'ccc-library-borrow-data');
     
     // Listen for changes in the data
-    onValue(booksRef, (snapshot) => {
+    onSnapshot(borrowDataCollection, () => {
         // Update the content whenever the data changes
-        fetchData();
+        fetchBorrowData();
     });
 }
 
